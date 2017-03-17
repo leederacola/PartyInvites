abf74fad38c3268283416db09be10ac2dea002c6
leederacola <rmgforce@gmail.com>
2017-03-17 11:36:38 -0400

    Made view, used some razor and learned bit about ViewBag

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -8,9 +8,10 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+       public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();
         }
     }
