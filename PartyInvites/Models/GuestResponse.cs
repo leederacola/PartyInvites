889fe8f202334a69ebff3c409f4fdc371cc76c29
leederacola <rmgforce@gmail.com>
2017-03-17 13:39:46 -0400

    added validation to rsvp form

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a valid email address")] // regular expression checks for formatting?
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter you phone number")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify weather you'll attend")]
         public bool? WillAttend { get; set; }
+
     }
 }
